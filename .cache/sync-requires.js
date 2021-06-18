1db3100b7296123a078ca55342a4a5da6710cd1a
Philippe Paris <philippe@ilogica.cl>
2021-06-18 10:16:51 -0400

    gh-pages

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -4,7 +4,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": preferDefault(require("/Users/philippeparis/Desktop/Ejercicio/pokemones/.cache/dev-404-page.js")),
-  "component---src-pages-index-jsx": preferDefault(require("/Users/philippeparis/Desktop/Ejercicio/pokemones/src/pages/index.jsx"))
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/philippeparis/Desktop/Ejercicio/pokemones/.cache/dev-404-page.js"))
 }
 
